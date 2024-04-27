f2c2ed9503a8d5dc52befb6817bd9aaf67540430
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-27 21:50:56 +0200

    Create unit tests for DiscountService

--- spec/services/discount_service_spec.rb
+++ spec/services/discount_service_spec.rb
@@ -1,20 +1,31 @@
-# spec/services/discount_service_spec.rb
+require 'rails_helper'
+
 RSpec.describe DiscountService do
-  it 'applies BOGO discounts correctly' do
-    basket = Basket.create!
-    tea = Product.create!(name: 'Green Tea', price: 3.11, bogo_eligible: true)
-    basket.basket_items.create!(product: tea, quantity: 2)
+  describe 'Discount application' do
+    let(:basket) { create(:basket) }
+    let(:green_tea) { create(:product, name: "Green Tea", code: "GR1", price: 3.11, bogo_eligible: true) }
+    let(:strawberries) { create(:product, name: "Strawberries", code: "SR1", price: 5.00, bulk_eligible: true, bulk_threshold: 3, new_price: 4.50) }
+    let(:coffee) { create(:product, name: "Coffee", code: "CF1", price: 11.23, bulk_eligible: true, bulk_threshold: 3, discount_factor: 2/3.0) }
 
-    DiscountService.new(basket).apply_discounts
-    expect(basket.basket_items.first.discounted_total_price).to eq(3.11)
-  end
+    it 'applies BOGO discount correctly for Green Tea' do
+      basket.basket_items.create(product: green_tea, quantity: 2)
+      DiscountService.new(basket).apply_discounts
+      item = basket.basket_items.first
+      expect(item.discounted_total_price).to eq(3.11)
+    end
 
-  it 'applies bulk discounts correctly' do
-    basket = Basket.create!
-    strawberries = Product.create!(name: 'Strawberries', price: 5.00, bulk_eligible: true, bulk_threshold: 3, bulk_discount_factor: 0.9)
-    basket.basket_items.create!(product: strawberries, quantity: 3)
+    it 'applies bulk discount with new price correctly for Strawberries' do
+      basket.basket_items.create(product: strawberries, quantity: 3)
+      DiscountService.new(basket).apply_discounts
+      item = basket.basket_items.first
+      expect(item.discounted_total_price).to eq(13.50)
+    end
 
-    DiscountService.new(basket).apply_discounts
-    expect(basket.basket_items.first.discounted_total_price).to eq(13.5)
+    it 'applies bulk discount with discount factor correctly for Coffee' do
+      basket.basket_items.create(product: coffee, quantity: 3)
+      DiscountService.new(basket).apply_discounts
+      item = basket.basket_items.first
+      expect(item.discounted_total_price).to eq(22.46)
+    end
   end
 end
