738f3b3d34906a07c2a419849de1c024fb394951
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-27 22:21:58 +0200

    Create DiscountService unit tests and update BasketItems model to set default discount price

--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -2,4 +2,14 @@ class BasketItem < ApplicationRecord
   belongs_to :basket
   belongs_to :product
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+  validates :discount_price, numericality: { greater_than_or_equal_to: 0 }
+
+  before_save :set_default_discount_price
+
+  private
+
+  def set_default_discount_price
+    # Initialize discount price only if it is not already set
+    self.discount_price ||= product.price
+  end
 end
--- spec/services/discount_service_spec.rb
+++ spec/services/discount_service_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe DiscountService do
   let(:strawberries) { create(:product, name: "Strawberries", code: "SR1", price: 5.00, bulk_eligible: true, bulk_threshold: 3, new_price: 4.50) }
   let(:coffee) { create(:product, name: "Coffee", code: "CF1", price: 11.23, bulk_eligible: true, bulk_threshold: 3, discount_factor: 2/3.0) }
 
-  it 'applies the correct discount price for BOGO eligible products' do
+  it 'applies BOGO discount correctly for Green Tea' do
     item = basket.basket_items.create(product: green_tea, quantity: 2)
     DiscountService.new(basket).apply_discounts
-    expect(item.reload.discount_price).to eq(3.11 / 2)
+    expect(item.reload.discount_price).to eq(3.11)
   end
 
-  it 'applies the correct discount price for bulk eligible products with new price' do
+  it 'applies bulk discount with new price correctly for Strawberries' do
     item = basket.basket_items.create(product: strawberries, quantity: 3)
     DiscountService.new(basket).apply_discounts
     expect(item.reload.discount_price).to eq(4.50)
   end
 
-  it 'applies the correct discount price for bulk eligible products with discount factor' do
+  it 'applies bulk discount with discount factor correctly for Coffee' do
     item = basket.basket_items.create(product: coffee, quantity: 3)
     DiscountService.new(basket).apply_discounts
-    expect(item.reload.discount_price).to eq(11.23 * 2/3.0)
+    expect(item.reload.discount_price).to eq(11.23 * 2 / 3.0)
   end
 end
