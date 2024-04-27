c55237836cda9aae2dad0c1e476c6146f27187be
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-27 17:01:05 +0200

    Add Shoulda Matchers gem and complete Basket and BasketItem model specs

--- spec/models/basket_item_spec.rb
+++ spec/models/basket_item_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BasketItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:basket) }
+  it { should belong_to(:product) }
+  it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(1) }
 end
--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Basket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:basket_items).dependent(:destroy) }
+  it { should have_many(:products).through(:basket_items) }
+
+  it 'is valid with valid attributes' do
+    basket = Basket.new
+    expect(basket).to be_valid
+  end
 end
