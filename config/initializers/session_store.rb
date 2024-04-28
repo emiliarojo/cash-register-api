3c2689436c4df93eaeeefe97432cfef4257ed12e
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-28 21:08:22 +0200

    Fix session store configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,11 @@
 if ENV["REDIS_URL"]
-  CashRegisterApi::Application.config.session_store :redis_store, {
-    servers: [{
-      url: ENV["REDIS_URL"],
-      serializer: :json
-    }],
-    expire_after: 90.minutes,
-    key: "_#{Rails.application.class.module_parent_name.downcase}_session"
+  Rails.application.config.session_store :redis_store, {
+    servers: [
+      {
+        url: ENV["REDIS_URL"],
+        namespace: "session"
+      },
+    ],
+    expire_after: 90.minutes
   }
 end
