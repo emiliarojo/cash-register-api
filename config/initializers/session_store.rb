6dfd6815348bdcc9677d1424204796d463001d9e
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-28 17:37:15 +0200

    Remove commented code in sessionb_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,3 @@
-# CashRegisterApi::Application.config.session_store :redis_store,
-#   servers: ["redis://localhost:6379/0/session"],
-#   expire_after: 90.minutes,
-#   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
-#   threadsafe: true,
-#   secure: true
-
 CashRegisterApi::Application.config.session_store :redis_store,
   servers: [
     {
