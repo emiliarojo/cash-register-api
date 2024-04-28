4eb7abfcf2ebb1ac0bcd790034f46c8dbbaaa521
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-28 17:35:50 +0200

    Change number of arguments in session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,11 @@
-CashRegisterApi::Application.config.session_store :redis_store, {
+# CashRegisterApi::Application.config.session_store :redis_store,
+#   servers: ["redis://localhost:6379/0/session"],
+#   expire_after: 90.minutes,
+#   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
+#   threadsafe: true,
+#   secure: true
+
+CashRegisterApi::Application.config.session_store :redis_store,
   servers: [
     {
       url: ENV['REDIS_URL'],
@@ -9,4 +16,3 @@ CashRegisterApi::Application.config.session_store :redis_store, {
   key: "_#{Rails.application.class.module_parent_name.downcase}_session",
   threadsafe: true,
   secure: true
-}
