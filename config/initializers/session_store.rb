6f48b41c3cdcc2f1f7ff7ecef530b7a69db8b352
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-28 22:26:34 +0200

    Fix production.rb to enable middleware

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,19 @@
-if ENV["REDIS_URL"]
-  Rails.application.config.session_store :redis_store, {
-    servers: [
-      {
-        url: ENV["REDIS_URL"],
-        namespace: "session"
-      },
-    ],
+# session_url = "#{ENV.fetch('REDIS_CACHE_URL', 'redis://127.0.0.1:6379')}/0/session"
+# secure = Rails.env.production?
+# key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
+# domain = ENV.fetch("DOMAIN_NAME", "localhost")
+
+# Rails.application.config.session_store :redis_store,
+#                                        url: session_url,
+#                                        expire_after: 90.minutes,
+#                                        key: key,
+#                                        domain: domain,
+#                                        threadsafe: true,
+#                                        secure: secure,
+#                                        same_site: :lax,
+#                                        httponly: true
+
+Rails.application.config.session_store :redis_store,
+    url: ENV['REDIS_URL'] || 'localhost:6379',
+    serializer: :json,
     expire_after: 90.minutes
-  }
-end
