55c0b525069313eff16e508f250891402b817f54
Emilia Rojo <emiliamrojo@gmail.com>
2024-04-29 18:20:38 +0200

    Add key to session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,2 @@
-Rails.application.config.session_store :redis_store, servers: [
-  {
-    url: ENV['REDIS_URL'],
-    namespace: 'session'
-  }
-], expire_after: 90.minutes
+# config/application.rb or config/initializers/session_store.rb
+Rails.application.config.session_store :redis_store, servers: [{ url: ENV['REDIS_URL'] }], expire_after: 90.minutes, key: '_your_app_session'
